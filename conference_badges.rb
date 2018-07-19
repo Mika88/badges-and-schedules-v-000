d0798ee3189188fe53e928d659c0bb1e1963f672
Mika88 <mikaspivak@gmail.com>
2018-07-19 20:04:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,10 +9,7 @@ end
 badge_m
 end
 def assign_rooms(attendees)
-  counter = 1
   attendees.each_with_index do |attendent, index|
-  attendees[attendent] = index
-  counter += 1
-     "Hello, #{index}! You'll be assigned to room #{counter}!"
+     "Hello, #{attendent}! You'll be assigned to room #{index + 1}!"
  end
  end
